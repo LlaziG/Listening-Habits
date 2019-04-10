c5d9c318a221f3b93cbe668f9428e60a2f71e237
LlaziG <lazar.sgeorgiev@yahoo.com>
2019-04-10 14:11:54 +0300

    Initial Client

---
+++ client-side/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://localhost:3001/api/user/authorize"
+            rel="noopener noreferrer"
+          >
+            Login with Spotify
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
